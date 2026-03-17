b94b713c7b5142a2fa4e9479bb7ca2608bdd9b79
ARUNKUMAR <arunmass4546@gmail.com>
2026-03-17 11:23:20 +0530

    Delete wrong directory

--- full_project/profile.py
+++
@@ -1,9 +0,0 @@
-#auth approve
-from fastapi import Depends
-from auth_handler import get_current_user
-
-def get_profile(user = Depends(get_current_user)):
-    return{
-        "message":"Authorised",
-        "user_name":user.name
-    }
